e44ccb27a21e3de2896839206c42cd9c7a59103e
Angel Nápoles Nápoles <angel.napolesqva@gmail.com>
2022-10-19 14:46:25 -0400

    Fixing models relation solicitud equipos

--- cliente/models.py
+++ cliente/models.py
@@ -20,3 +20,12 @@ class Cliente(models.Model):
     def __str__(self):
         return f"Cliente({self.nombre} {self.apellidos} {self.telefono})"
 
+
+class Equipo(models.Model):
+    cliente = models.ForeignKey(Cliente, related_name="equipos", on_delete=models.CASCADE)
+    nombre = models.CharField(blank=False, null=False, max_length=50)
+    marca = models.CharField(blank=False, null=False, max_length=50)
+    ubicacion = models.CharField(blank=False, null=False, max_length=100)
+
+    def __str__(self):
+        return f"Equipo({self.nombre}, {str(self.cliente)})"
--- equipo/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class EquipoConfig(AppConfig):
-    name = 'equipo'
